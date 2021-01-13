366c79cfb47efa85ade46c989fd7f861e1473c25
lshiencodia <lshi@encodia.com>
2021-01-12 19:06:17 -0800

    Created subfolders

--- 1_pick_fragment.sh
+++
@@ -1,6 +0,0 @@
-~/git_rosetta/Rosetta/main/source/bin/fragment_picker.default.linuxgccrelease @flags_picker
-~/git_rosetta/Rosetta/main/source/bin/fragment_picker.boost_thread.linuxgccrelease @flags -j 10
-
-~/git_rosetta/Rosetta/main/source/bin/extract_pdbs.default.linuxgccrelease -database ~/git_rosetta/Rosetta/main/database/ -in:file:silent avb6B.fasta.frags.*.8mers.out
-
-vall pdbs in /work/dekim/pdb/pdb
--- check_abego.sh
+++
@@ -1,2 +0,0 @@
-~/git_rosetta/Rosetta/main/source/bin/rama.default.linuxgccrelease -database ~/git_rosetta/Rosetta/main/database/ -s $1 -abego 4 -output $1_rama3.dat
-
