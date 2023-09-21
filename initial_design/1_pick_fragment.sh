d0d851872d1a25111cb64ad1f6344e200efa9593
Fuming Zeng <1097717610@qq.com>
2023-09-21 11:01:23 +0800

    Update 1_pick_fragment.sh

--- initial_design/1_pick_fragment.sh
+++ initial_design/1_pick_fragment.sh
@@ -1,6 +1,8 @@
 ~/git_rosetta/Rosetta/main/source/bin/fragment_picker.default.linuxgccrelease @flags_picker
+# 运行fragment_picker.default.linuxgccrelease工具，使用flags_picker文件内的参数作为输入
 ~/git_rosetta/Rosetta/main/source/bin/fragment_picker.boost_thread.linuxgccrelease @flags -j 10
 
 ~/git_rosetta/Rosetta/main/source/bin/extract_pdbs.default.linuxgccrelease -database ~/git_rosetta/Rosetta/main/database/ -in:file:silent avb6B.fasta.frags.*.8mers.out
 
 vall pdbs in /work/dekim/pdb/pdb
+
